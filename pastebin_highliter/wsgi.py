d1236c395171ba849e3e4d39e4a69a4e2ad21af4
Andres Reyna <andresreyna15@gmail.com>
2023-12-25 02:38:21 -0600

    First commit.

---
+++ pastebin_highliter/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pastebin_highliter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin_highliter.settings')
+
+application = get_wsgi_application()
