d916630c05957ec7fc2ffbb11ea7f6ce86204e15
Andres Reyna <andresreyna15@gmail.com>
2023-12-27 01:20:03 -0600

    Part 5. Done

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
-    
+    owner = serializers.ReadOnlyField(source="owner.username") 
     # This simple shortcut replaces all previous code, definitions and functions
     class Meta:
         model = Snippet
         # On serializers, as opposed to Django Models, the '__all__' string doesn't indicate to use all fields
         # it only requires to specifie the model deinition (model = Snippet) to serialize all fields,
         # only use fields attribute whenever specific attributes are going to be serialized.
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style') 
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner') 
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,12 @@
-from rest_framework import status, generics
+from django.contrib.auth.models import User
+
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 from rest_framework import serializers
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 def home():
@@ -13,6 +16,10 @@ def home():
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [ permissions.IsAuthenticatedOrReadOnly ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -20,3 +27,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [ permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly ]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
