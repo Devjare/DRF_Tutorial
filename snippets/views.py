b3e427e655bd494f679a5662ce0de29d7e959e65
Andres Reyna <andresreyna15@gmail.com>
2023-12-26 23:36:30 -0600

    Finished part 3. Generic views

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,6 @@
-from django.http import Http404
-
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
 
 from .models import Snippet
 from .serializers import SnippetSerializer
@@ -14,35 +10,13 @@ def home():
     return HttpResponse("<h5>Home</h5>")
 
 # Similar to @app.route("/", methods=[] from flask.
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a code snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
